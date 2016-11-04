27290c5ce944f3aa6a5aa18b93d1adc905091265
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-11-04 15:06:39 +0100

    Configured base layout, put NavBar into partial and created BaseViewModel.

--- ACLager/Controllers/HomeController.cs
+++ ACLager/Controllers/HomeController.cs
@@ -3,23 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ACLager.Models.ViewModels;
 
 namespace ACLager.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
-        }
-
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            BaseViewModel viewModel = new BaseViewModel {RenderNavBar = false};
+            return View(viewModel);
         }
     }
 }

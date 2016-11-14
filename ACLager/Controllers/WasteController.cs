c1689c76162671a9a7dd037720acad36327ed990
Jens Burla Johansen <jens@burla.dk>
2016-11-14 16:05:33 +0100

    Added controllers.

---
+++ ACLager/Controllers/WasteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACLager.Controllers
+{
+    public class WasteController : Controller
+    {
+        // GET: Waste
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

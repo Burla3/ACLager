eef2a96c1fbd97c0ad06b96d602c33279dea04a2
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-11-16 09:24:44 +0100

    Added two ekstra properties to HomeMenuBlock.

--- ACLager/CustomClasses/HomeMenuBlock.cs
+++ ACLager/CustomClasses/HomeMenuBlock.cs
@@ -5,14 +5,18 @@ using System.Web;
 
 namespace ACLager.CustomClasses {
     public class HomeMenuBlock {
-        public HomeMenuBlock(string color, string title, string description) {
+        public HomeMenuBlock(string color, string title, string description, string controller, string action) {
             Color = color;
             Title = title;
             Description = description;
+            Controller = controller;
+            Action = action;
         }
 
         public string Color { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
     }
 }

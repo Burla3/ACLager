b77a74377851ebdd1f1f5f9d20ae43d3070c93e9
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-11-16 20:38:02 +0100

    Clean up after merge.

--- ACLager/Controllers/UserController.cs
+++ ACLager/Controllers/UserController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ACLager.Interfaces;
 using ACLager.Models;
 using ACLager.ViewModels;
 
 namespace ACLager.Controllers
 {
-    public class UserController : Controller
+    public class UserController : Controller, ILoggable
     {
         private readonly ACLagerDatabaseEntities _db = new ACLagerDatabaseEntities();
 
@@ -22,7 +23,7 @@ namespace ACLager.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(User user) {
+        public ActionResult CreateUser(User user) {
 
             //if (!ModelState.IsValid) {
             //    IEnumerable<User> users = from userdb in _db.Users
@@ -36,5 +37,40 @@ namespace ACLager.Controllers
 
             return Redirect("/User");
         }
+
+        /// <summary>
+        /// Edit an employee by updating the name, isActive and isAdmin.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="isActive"></param>
+        /// <param name="isAdmin"></param>
+        /// <returns>Returns true if successful.</returns>
+        public bool EditEmployee(string name, bool isActive, bool isAdmin) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all employees in an IEnumerable.
+        /// </summary>
+        /// <returns>All employees in an IEnumerable</returns>
+        public IEnumerable<User> GetEmployees()
+         {
+             throw new NotImplementedException();
+         }
+ 
+ 
+         /// <summary>
+         /// Deletes an existing employee.
+         /// </summary>
+         /// <param name="uid"></param>
+         /// <returns>Returns true if successful.</returns>
+         public bool DeleteEmployee(long uid)
+         {
+             throw new NotImplementedException();
+         }
+ 
+     
+ 
+         public event ChangedEventHandler Changed;
     }
 }

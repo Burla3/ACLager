a62ebccec68e952fb4de8b22156a9bef419fc997
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-11-16 10:38:22 +0100

    Added missing controllers and index view to those.

---
+++ ACLager/Controllers/ItemTypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACLager.Controllers
+{
+    public class ItemTypeController : Controller
+    {
+        // GET: ItemType
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

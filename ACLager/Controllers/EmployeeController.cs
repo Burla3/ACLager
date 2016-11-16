749f7d2f8a3dad66ae8258391150a624f49e1c56
Creep <jonasenghave@gmail.com>
2016-11-16 14:56:35 +0100

    Added XML documentation to EmployeeController

--- ACLager/Controllers/EmployeeController.cs
+++ ACLager/Controllers/EmployeeController.cs
@@ -15,26 +15,45 @@ namespace ACLager.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Gets all employees in an IEnumerable.
+        /// </summary>
+        /// <returns>All employees in an IEnumerable</returns>
         public IEnumerable<User> GetEmployees()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Creates a new employee.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Returns true if successful.</returns>
         public bool CreateEmployee(string name)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Deletes an existing employee.
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <returns>Returns true if successful.</returns>
         public bool DeleteEmployee(long uid)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Changes an employee by updating the name, isActive and isAdmin.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="isActive"></param>
+        /// <param name="isAdmin"></param>
+        /// <returns>Returns true if successful.</returns>
         public bool ChangeEmployee(string name, bool isActive, bool isAdmin)
         {
             throw new NotImplementedException();
-        }
-
-        
+        }      
     }
 }

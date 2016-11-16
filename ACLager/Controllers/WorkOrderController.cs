6007883fa79f8f32a8089277309f065ee34284a6
Creep <jonasenghave@gmail.com>
2016-11-16 14:31:43 +0100

    Added WorkOrderController skeleton

--- ACLager/Controllers/WorkOrderController.cs
+++ ACLager/Controllers/WorkOrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ACLager.Models;
 
 namespace ACLager.Controllers
 {
@@ -12,6 +13,46 @@ namespace ACLager.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        
+        public IEnumerable<WorkOrder> GetWorkOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CancelWorkOrder(WorkOrder workOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CancelWorkOrderItem(WorkOrderItem workOrderItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateWorkOrder(DateTime dueDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateWorkOrderItem(long amount, long progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CompleteWorkOrder(WorkOrder workOrder, User completedBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateWorkOrder(DateTime dueDate, string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateWorkOrderItem(WorkOrder workOrder, ItemType itemType, long amount)
+        {
+            throw new NotImplementedException();
         }
     }
 }

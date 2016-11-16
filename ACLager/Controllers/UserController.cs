00ea950ddfeeb908ff5a28812cc3c3c7a91b1f3e
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-11-16 20:08:49 +0100

    Renamed files with employee to user. and added UserViewModel.

---
+++ ACLager/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ACLager.Models;
+using ACLager.ViewModels;
+
+namespace ACLager.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ACLagerDatabaseEntities _db = new ACLagerDatabaseEntities();
+
+        // GET: Employee
+        [HttpGet]
+        public ActionResult Index() {
+            IEnumerable<User> users = from user in _db.Users
+                                        select user;
+
+            return View(new UserViewModel(users, new User()));
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user) {
+
+            //if (!ModelState.IsValid) {
+            //    IEnumerable<User> users = from userdb in _db.Users
+            //                              select userdb;
+
+            //    return View(new UserViewModel(users, new User()));
+            //}
+
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            return Redirect("/User");
+        }
+    }
+}
---
+++ ACLager/ViewModels/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ACLager.Models;
+
+namespace ACLager.ViewModels {
+    public class UserViewModel : BaseViewModel {
+        public UserViewModel() {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="users"></param>
+        /// <param name="user"></param>
+        public UserViewModel(IEnumerable<User> users, User user) {
+            Users = users;
+            User = user;
+        }
+
+        public User User { get; set; }
+        public IEnumerable<User> Users { get; set; }
+    }
+}

56f70dffd7d5c0ce15633c0ecebba78836e1b526
Daniel Johnsen <daniel.oestergaard.johnsen@gmail.com>
2016-11-16 15:01:12 +0100

    Skelet til location controller lavet

--- ACLager/Controllers/LocationController.cs
+++ ACLager/Controllers/LocationController.cs
@@ -13,5 +13,24 @@ namespace ACLager.Controllers
         {
             return View();
         }
+        /// <summary>
+        /// Creates a new location 
+        /// </summary>
+        /// <param name="newLocationId">ID of the to be created location</param>
+        /// <returns>Whether or not the creation was successful</returns>
+        public bool CreateLocation(long newLocationId)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Deletes an existing location
+        /// </summary>
+        /// <param name="locationId">ID of the to be deleted location</param>
+        /// <returns>Whether or not the deletion was successful</returns>
+        public bool DeleteLocation(long locationId)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }

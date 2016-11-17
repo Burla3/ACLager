848a3a163e2fd35f2e05b5b0bc91ec8a088419ab
Daniel Johnsen <daniel.oestergaard.johnsen@gmail.com>
2016-11-17 15:17:18 +0100

    AddItemType given a body (reverted from commit 6bfd2514e9de88e3429a6fd11b43da9b13d2d93e)

--- ACLager/Controllers/ItemTypeController.cs
+++ ACLager/Controllers/ItemTypeController.cs
@@ -9,7 +9,6 @@ namespace ACLager.Controllers
 {
     public class ItemTypeController : Controller
     {
-        private readonly ACLagerDatabaseEntities _db = new ACLagerDatabaseEntities();
         // GET: ItemType
         public ActionResult Index()
         {
@@ -23,32 +22,7 @@ namespace ACLager.Controllers
         /// <returns>true if successful</returns>
         public bool AddItemType(ItemType itemType)
         {
-            var exsistingItemType = (from itemT in _db.ItemTypes where itemT.name == itemType.name select itemT).First();
-
-            //An item type with this name exsist, but uses another unit of measurement
-            if(exsistingItemType != null && itemType.unit != exsistingItemType.unit)
-            {
-                _db.ItemTypes.Add(itemType);
-                _db.SaveChanges();
-                return true;
-            }
-            //An item type with this name does not exist if existingItemType == null
-            else if (exsistingItemType == null)
-            {
-                _db.ItemTypes.Add(itemType);
-                _db.SaveChanges();
-                return true;
-            }
-            //An item with the same name and unit of measurement already exsists
-            else
-            {
-                if (itemType.is_active==false)
-                {
-                    //Hey, it's there it's just inactive
-                }
-                
-                return false;
-            }
+            throw new NotImplementedException();
         }
 
         /// <summary>

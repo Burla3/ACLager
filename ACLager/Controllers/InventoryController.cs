32aa574b7a35565d910d2d76f90676caab2a8a98
Jalf <benjaminboendergaard@hotmail.com>
2016-11-18 15:22:03 +0100

    Updated InventoryController Index to use the new usage of the database connection. And replaced Tuple with the new ItemLocationPair class.

--- ACLager/Controllers/InventoryController.cs
+++ ACLager/Controllers/InventoryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,36 +22,26 @@ namespace ACLager.Controllers
         // GET: Inventory
         public ActionResult Index()
         {
-            ACLagerDatabaseEntities db = new ACLagerDatabaseEntities();
-            //db.Items.Add(new Item("Chokolademus", 100, new DateTime(2020, 12, 17), DateTime.Now, "AC" ));
-            db.SaveChanges();
-
-            IEnumerable<ItemType> itemTypes = from itemType in db.ItemTypes
-                                                where itemType.is_active
-                                                select itemType;
-
-            IEnumerable<Item> items = from item in db.Items
-                                        where item.amount > 0
-                                        select item;
-
-            IEnumerable<Location> locations = from location in db.Locations
-                                                where location.is_active
-                                                select location;
-
             List<ItemGroup> itemGroups = new List<ItemGroup>();
 
-            foreach (ItemType itemType in itemTypes) {
-                List<Tuple<Item, Location>> itemAndLocations = new List<Tuple<Item, Location>>();
+            using (ACLagerDatabaseEntities db = new ACLagerDatabaseEntities())
+            {
+                IEnumerable<Item> items = db.Items.Where(i => i.amount > 0);
+                IEnumerable<ItemType> itemTypes = db.ItemTypes.Where(it => it.is_active);
+                IEnumerable<Location> locations = db.Locations.Where(l => l.is_active);
 
-                foreach (Item item in items.Where(i => itemType.uid == i.item_type)) {
-                    itemAndLocations.Add(new Tuple<Item, Location>(item, locations.Single(l => l.uid == item.location)));
+                foreach (ItemType itemType in itemTypes)
+                {
+                    List<ItemLocationPair> itemLocationPairs = new List<ItemLocationPair>();
+
+                    foreach (Item item in items.Where(i => itemType.uid == i.item_type))
+                    {
+                        itemLocationPairs.Add(new ItemLocationPair(item, locations.Single(l => l.uid == item.location)));
+                    }
+
+                    itemGroups.Add(new ItemGroup(itemType, itemLocationPairs));
                 }
-
-                
-                itemGroups.Add(new ItemGroup(itemType, itemAndLocations));
             }
-
-            
 
             return View(new InventoryViewModel(itemGroups));
         }

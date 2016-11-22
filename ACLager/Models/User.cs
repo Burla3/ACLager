f0cebf3ed98a5bdeeeba204dede6075642b65b03
Jens Burla Johansen <jens@burla.dk>
2016-11-22 15:20:32 +0100

    Changed default value of IsActive on User class.

--- ACLager/Models/User.cs
+++ ACLager/Models/User.cs
@@ -17,6 +17,7 @@ namespace ACLager.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public User()
         {
+            this.IsActive = true;
             this.WorkOrders = new HashSet<WorkOrder>();
             this.WasteReports = new HashSet<WasteReport>();
         }

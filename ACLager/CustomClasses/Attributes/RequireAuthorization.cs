1cd0aa6bda42e68357cecfc0f56e8b0b1d8da9f0
Jens Burla Johansen <jens@burla.dk>
2016-11-24 09:41:48 +0100

    Created login feature including attributes, controller, viewmodel and more.

---
+++ ACLager/CustomClasses/Attributes/RequireAuthorization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using ACLager.Controllers;
+
+namespace ACLager.CustomClasses.Attributes {
+    public class RequireAuthorization : ActionFilterAttribute {
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            bool redirect = false;
+            bool cookieExists = filterContext.HttpContext.Request.Cookies.AllKeys.Contains("UserInfo");
+
+            if (cookieExists) {
+                HttpCookie cookie = filterContext.HttpContext.Request.Cookies["UserInfo"];
+
+                if (cookie.Value != "LoggedOut" && cookie.Value != String.Empty) {
+                    // Do nothing
+                } else {
+                    redirect = true;
+                }
+            } else {
+                redirect = true;
+            }
+
+            if (redirect) {
+                filterContext.Result = new RedirectResult("/Login?nextAction=" + HttpUtility.UrlEncode(filterContext.HttpContext.Request.Url.AbsolutePath));
+            }
+        }
+    }
+}
---
+++ ACLager/ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ACLager.Models;
+
+namespace ACLager.ViewModels {
+    public class LoginViewModel : BaseViewModel {
+        public User User { get; set; } = new User();
+        public bool RenderUserNotFoundWarning { get; set; } = false;
+    }
+}

0f2d1899a73176951c38a87e3a7ee7f126b0320a
Jens Burla Johansen <jens@burla.dk>
2016-11-29 11:10:19 +0100

    Modified HomeController so HomeMenuBlocks only are rendered when they should.

--- ACLager/Controllers/HomeController.cs
+++ ACLager/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,21 +10,49 @@ using ACLager.ViewModels;
 namespace ACLager.Controllers {
     public class HomeController : Controller {
         public List<HomeMenuBlock> homeMenuBlocks = new List<HomeMenuBlock> {
-                new HomeMenuBlock("purple", "Lagerstyring", "Noget om at man kan plukke, søge, indsætte og hvad man nu ellers kan.", "Inventory", "Index"),
-                new HomeMenuBlock("brown", "Produktion", "Ordrer til produktionsafdelingen.", "WorkOrder", "Production"),
-                new HomeMenuBlock("orange", "Pakkeri", "Ordrer til pakkeriet.", "WorkOrder", "Packaging"),
-                new HomeMenuBlock("deep-orange", "Spildrapport", "Rapportering af spild.", "Waste", "Index"),
-                new HomeMenuBlock("amber", "Brugere", "Håndtering af brugere", "User", "Index"),
-                new HomeMenuBlock("blue", "Lokationer", "Håndtering af lokationer", "Location", "Index"),
-                new HomeMenuBlock("cyan", "Aktivitetshistorik", "Se aktivitetshistorik", "Log", "Index"),
-                new HomeMenuBlock("green", "Oversigt over spild", "Se spildrapporter", "Waste", "OverView"),
-                new HomeMenuBlock("indigo", "Artikel typer", "Håndtere artikel typer.", "ItemType", "Index")
+                new HomeMenuBlock("purple", "Lagerstyring", "Tilføj, rediger og pluk lagervarer.", "Inventory", "Index", false),
+                new HomeMenuBlock("brown", "Produktion", "Ordrer til produktionen.", "WorkOrder", "Production", false),
+                new HomeMenuBlock("orange", "Pakkeri", "Ordrer til pakkeriet.", "WorkOrder", "Packaging", false),
+                new HomeMenuBlock("deep-orange", "Spildrapport", "Rapportering af spild.", "Waste", "Index", false),
+                new HomeMenuBlock("amber", "Brugere", "Håndtering af brugere.", "User", "Index", true),
+                new HomeMenuBlock("indigo", "Varetyper", "Håndtering af varetyper.", "ItemType", "Index", true),
+                new HomeMenuBlock("blue", "Lokationer", "Håndtering af lokationer.", "Location", "Index", true),
+                new HomeMenuBlock("cyan", "Aktivitetshistorik", "Se aktivitetshistorik.", "Log", "Index", true),
+                new HomeMenuBlock("green", "Oversigt over spild", "Se spildrapporter.", "Waste", "OverView", true)
             };
-        public ActionResult Index() {
 
-            HomeViewModel viewModel = new HomeViewModel {HomeMenuBlocks = homeMenuBlocks};
+        public ActionResult Index()
+        {
+            List<HomeMenuBlock> renderedHomeMenuBlocks = new List<HomeMenuBlock>();
+
+            HttpCookie cookie = HttpContext.Request.Cookies["UserInfo"];
+
+            dynamic cookieData = System.Web.Helpers.Json.Decode(cookie.Value);
+            bool isAdmin = cookieData["IsAdmin"];
+
+            foreach (HomeMenuBlock homeMenuBlock in homeMenuBlocks)
+            {
+                bool render = false;
+
+                if (!homeMenuBlock.IsAdminOnly)
+                {
+                    render = true;
+                } else if (homeMenuBlock.IsAdminOnly && isAdmin)
+                {
+                    render = true;
+                }
+                else
+                {
+                    // Do not render
+                }
+
+                if (render)
+                {
+                    renderedHomeMenuBlocks.Add(homeMenuBlock);
+                }
+            }
             
-            return View(viewModel);
+            return View(new HomeViewModel {HomeMenuBlocks = renderedHomeMenuBlocks});
         }
     }
 }

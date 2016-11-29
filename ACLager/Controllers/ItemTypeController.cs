f773a8bb281d6bba3a954ee9f5f9431e5221dd58
Jens Burla Johansen <jens@burla.dk>
2016-11-29 11:09:11 +0100

    Implemented AdminOnly attribute and redirection and added it for some controllers.

--- ACLager/Controllers/ItemTypeController.cs
+++ ACLager/Controllers/ItemTypeController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ACLager.CustomClasses.Attributes;
 using ACLager.Models;
 
 namespace ACLager.Controllers
 {
+    [AdminOnly]
     public class ItemTypeController : Controller
     {
         // GET: ItemType

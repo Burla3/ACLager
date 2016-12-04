bdb3e44188a1309adb11a335fe12d01223fa3b4e
Hangover1234 <joa.theis@gmail.com>
2016-12-04 23:31:54 +0100

    createwastereport beskrivende text ser lidt bedre ud nu

--- ACLager/Controllers/WasteController.cs
+++ ACLager/Controllers/WasteController.cs
@@ -60,7 +60,7 @@ namespace ACLager.Controllers
                 workorder = db.WorkOrderSet.ToList();
 
                 foreach(Item item in items){
-                    SelectItemList.Add(new SelectListItem { Text = $"ID: {item.UID} Lokation: {item.Location.Name} Enhed: {item.ItemType.Unit} Navn: {item.ItemType.Name}", Value = item.UID.ToString() });
+                    SelectItemList.Add(new SelectListItem { Text = $"Lokation: {item.Location.Name} || Enhed: {item.ItemType.Unit} || Navn: {item.ItemType.Name}", Value = item.UID.ToString() });
                 }
 
                 foreach (WorkOrder work in workorder){
